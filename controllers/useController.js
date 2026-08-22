769e01ad2a2ac15bc2cc16a7b13e18ed721dd125
yashashm <yashashm19@gmail.com>
2026-08-22 21:57:03 +0530

    hi

--- controllers/useController.js
+++ controllers/useController.js
@@ -1,38 +1,126 @@
-
+import User from "../model/userSchema.js"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 // login
 // logout
 // signup
 // profie
 
-export const login = async (req,res)=>{
-    try{
-        const {name,age,email,password}=req.body
 
-        if(!name||!password ||!email){
-           return req.json({
-                message:"Some fileds are missing"
-            })
-        }
+//Payload,secret key,expires
+const createToken=(id,email)=>{
+    if(! process.env.JWT_KEY){
+        throw new Error("JWT Secret key is missing")
     }
-    catch{
-
-    }
+    jwt.sign(
+        {id,email},
+        process.env.JWT_KEY,
+        {expiresIn:"1h"})
 }
 
 
-export const logout = async (req,res)=>{
+//Cookies option
+const cookieOption={
+    httpOnly:true,
+    secure:false,
+    maxAge:60*60*1000
+}
+
+
+export const signup = async (req,res)=>{
     try{
+        const {name,age,email,password}=req.body
+
+        if(!name||!password ||!email|| !age){
+           return res.status(400).json({
+                message:"Some fileds are missing"
+            })
+        }
+        //Same email wala exist nahi karta
+        const user=await User.findOne({email});
+        if(user){
+            return res.status(409).json({
+                message:"Email already exists"
+            })
+        }
+        const hashPassword=await bcrypt.hash(password,12);
+
+        const userCreated= await User.create({
+            name,age,email,password:hashPassword
+        });
+
+        //create token
+        const token=createToken(userCreated._id,email);
+        
+        res.cookie("token",token,cookieOption);
+
+        res.status(201).json({
+            message:"User Created succesfully",
+            name,age,email
+        });
 
     }
-    catch{
+    catch(err){
+        console.log(err);
+        res.status(500).json({
+            message:"Internal Error"
+        })
         
     }
 }
 
-export const signup = async (req,res)=>{
-    try{
 
+export const login  = async (req,res)=>{
+    try{
+        const {email,password}=req.body;
+        if(!password ||!email){
+           return res.status(400).json({
+                message:"Some fileds are missing"
+            })
+        }
+
+        //verify Password
+        const existingUser = await User.findOne({email});
+
+        if(!existingUser){
+            return res.status(401).json({
+                message:"Invalid Credtials"
+            })
+        };
+
+       
+       const isMatch=await bcrypt.compare(password,existingUser.password);
+
+        if(!isMatch){
+            return res.status(401).json({
+                message:"Invalid Credtials"
+            })
+        }
+        const token=createToken(existingUser._id,email);
+
+        res.cookie("token",token,cookieOption);
+
+        res.status(200).json({
+            message:"User Logged in SuccessFully",
+            name: existingUser.name,
+            age: existingUser.age,
+            email: existingUser.email,
+            usage: existingUser.usage
+        });
+    }
+    catch{
+        console.log(err);
+        res.status(500).json({
+            message:"Internal Error"
+        })
+    }
+}
+
+ 
+export const logout = async (req,res)=>{
+    try{
+          
     }
     catch{
         
